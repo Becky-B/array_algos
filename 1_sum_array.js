3166d642f1eae995702923b7c6a87c36033a300b
Becky_B <bettsrebecca@yahoo.com>
2020-02-19 13:14:18 -0800

    1_sum_array.js

--- 1_sum_array.js
+++ 1_sum_array.js
@@ -3,3 +3,12 @@
 // When you have solved this, create new values for your array and ensure it still works as expected.
 
 // YOUR CODE HERE
+//
+var arr=[6,3,5,1,2,4]
+var sum= 0
+var Num
+    for(num=0; num < arr.length; num++){
+        console.log("num"+ arr[num]);
+    sum+=arr[num]
+        console.log("sum"+ sum)
+    }
